ff356e196ce44bb5559b99ff129aba5de3367950
kristinaNoskova <121613581+kristinaNoskova@users.noreply.github.com>
2023-08-16 15:52:16 +0300

    added modal

--- main.js
+++ main.js
@@ -10,8 +10,27 @@ document.addEventListener('mouseup', (e) => {
 
   cartButton.addEventListener('click', () => {
     popover.classList.toggle('popover-open');
-
   })
 })
 
 // modal
+const deliveryBtn = document.querySelector('.delivery-btn');
+const modalCloseButton = document.querySelector('.modal-content__close-btn');
+const modalContainer = document.querySelector('.modal-container');
+const modal = document.querySelector('.modal');
+
+
+deliveryBtn.addEventListener('click', () => {
+  modalContainer.classList.remove('modal-container-close');
+})
+
+modalCloseButton.addEventListener('click', () => {
+  modalContainer.classList.add('modal-container-close');
+})
+
+modalContainer.addEventListener('mouseup', (e) => {
+  const click = e.composedPath().includes(modal);
+  if (!click) {
+    modalContainer.classList.add('modal-container-close');
+  }
+})
